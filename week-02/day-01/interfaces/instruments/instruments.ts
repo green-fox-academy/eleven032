5f6ade87f446f3f63ab1c65d8a35dc92e44e961e
eleven032 <eleven032@gmail.com>
2019-09-23 10:48:02 +0800

    waiting for problem fix

--- week-02/day-01/interfaces/instruments/instruments.ts
+++ week-02/day-01/interfaces/instruments/instruments.ts
@@ -3,14 +3,50 @@ abstract class Instrument {
     constructor(name: string) {
         this.name = name;
     };
-    abstract play(): any;
+    abstract play(): string;
 }
 
 abstract class StringedInstrument extends Instrument {
-    public numberOfStrings: number;
-    abstract sound(): any;
-    play(){
-        
+    numberOfStrings:number;
+    constructor(name:string, numberOfStrings:number){
+        super(`${name}`);
+        this.numberOfStrings = numberOfStrings;
+    }
+    abstract sound(): string;
+    play():string{
+        return `${this.name}, a ${this.numberOfStrings}-stringed instrument that goes ${this.sound()} `;
     }
 }
 
+class ElectricGuitar extends StringedInstrument{
+    constructor(numberOfStrings:number = 6){
+        super('Electric Guitar', numberOfStrings);
+        this.numberOfStrings = numberOfStrings;
+    }
+    
+    sound(): string {
+        return 'Twang';
+    }
+}
+
+class BassGuitar extends StringedInstrument{
+    constructor(numberOfStrings:number = 4){
+        super('Bass Guitar', numberOfStrings);
+        this.numberOfStrings = numberOfStrings;
+    }
+    
+    sound(): string {
+        return 'Duum-duum-duum';
+    }
+}
+
+class Violin  extends StringedInstrument{
+    constructor(numberOfStrings:number = 4){
+        super('Violin ', numberOfStrings);
+        this.numberOfStrings = numberOfStrings;
+    }
+    
+    sound(): string {
+        return 'Screech';
+    }
+}
