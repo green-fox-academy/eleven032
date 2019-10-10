e6a91f2772f6a7e0af00ab310a90d14209525dfa
大多 <eleven032@gmail.com>
2019-10-10 18:00:17 +0800

    can add by interval

--- week-03/day-04/candy-game/game.js
+++ week-03/day-04/candy-game/game.js
@@ -5,3 +5,41 @@
 //   - Use the 🍭 emoji to display the lollipops you have
 // - Display the candy producton rate in the `Candies / Second` row
 // - If you press the "make candy rain" button, the candy generation should speed up 10x
+
+const createCandy = document.querySelector('.create-candies');
+
+const buyLollypops = document.querySelector('.buy-lollypops');
+
+const candyMachine = document.querySelector('.candy-machine');
+
+const lollypop = document.querySelector('.lollypops').innerHTML.slice(0, 2);
+
+createCandy.addEventListener('click', (event) => {
+    let count = parseInt(document.querySelector('.candies').innerHTML);
+    count++;
+    document.querySelector('.candies').innerHTML = count;
+})
+
+buyLollypops.addEventListener('click', (event) => {
+    let count = parseInt(document.querySelector('.candies').innerHTML);
+    if (count >= 100) {
+        document.querySelector('.candies').innerHTML -= count;
+        document.querySelector('.lollypops').innerHTML += lollypop;
+    } else {
+        console.log('No enough candies');
+    }
+})
+
+
+setInterval(function () {
+    let count = document.querySelector('.lollypops').innerHTML.length / 2;
+    console.log(count);
+    if (count >= 10) {
+        let first = 0;
+        for (let i = 0; i < count.toString().length; i++) {
+            count = count / 10;
+        }
+        first = count;
+        document.querySelector('.candies').innerHTML += first.toString();
+    }
+}, 1000);
