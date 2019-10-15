b7bfaee2bafb1f179a8bd92fe96995a8b8a37513
大多 <eleven032@gmail.com>
2019-10-15 11:33:26 +0800

    hellow world done

--- week-04/day-02/hello-world/hello_world/src/App.js
+++ week-04/day-02/hello-world/hello_world/src/App.js
@@ -3,7 +3,8 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link
+  Link,
+  useParams
 } from "react-router-dom";
 import './App.css';
 
@@ -32,11 +33,14 @@ function App() {
           <Route exact path="/">
             <Home />
           </Route>
-          <Route path="/about">
-            <About />
+          <Route exact path="/error">
+            <Error />
           </Route>
-          <Route path="/dashboard">
-            <Dashboard />
+          <Route path="/error/:code">
+            <ErrorCode />
+          </Route>
+          <Route path="/:any">
+            <Imp />
           </Route>
         </Switch>
       </div>
@@ -48,6 +52,31 @@ function Home() {
   return (
     <div>
       <h2>Hello World!</h2>
+    </div>
+  );
+}
+
+function Error() {
+  return (
+    <div>
+      <h2>Something bad happened.</h2>
+    </div>
+  );
+}
+
+function ErrorCode() {
+  let { code } = useParams();
+  return (
+    <div>
+      <h2>Error: {code}</h2>
+    </div>
+  );
+}
+
+function Imp() {
+  return (
+    <div>
+      <h2>This page is not implemented yet </h2>
     </div>
   );
 }
