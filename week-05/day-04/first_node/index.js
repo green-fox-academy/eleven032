bf65d6c2e7d723e06af72f67c8a4de2b46fa3943
大多 <eleven032@gmail.com>
2019-10-24 10:03:41 +0800

    week 5 day4 exe1 done

--- week-05/day-04/first_node/index.js
+++ week-05/day-04/first_node/index.js
@@ -1,1 +1,15 @@
-console.log('It is working');
+const express = require('express');
+const app = express();
+const PORT = 3000
+
+// app.get('/', (request, response => {
+//     response.send('Hello EPAM team!')
+// }))
+
+app.listen(PORT, () => {
+    console.log(`Now listen at ${PORT}`);
+})
+
+app.get('/',(req,res)=>{
+    res.sendFile(__dirname+'/index.html');
+})
