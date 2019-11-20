76a8984c12fdadb84473fc5b502a4aa7d903f4da
大多 <eleven032@gmail.com>
2019-11-20 13:42:09 +0800

    set up example

---
+++ group-project/component-sample/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import AccountLists from './components/AccountLists';
+import { View } from 'react-native';
+export default function App() {
+  return (
+    <View>
+      <AccountLists />
+    </View>
+  );
+}
